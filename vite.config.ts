601f162785016eb4c8c4b48d4c5ac5e9a55127f9
Egle Dirmeityte <dirmeityteegle@gmail.com>
2024-06-02 17:33:47 +0300

    ci: remove deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: process.env.GITHUB_REPOSITORY
-    ? `/${process.env.GITHUB_REPOSITORY.split("/")[1]}/`
-    : "/",
   plugins: [react()],
   server: {
     watch: {
